c397818dab78d14b5434bfb1fdefd9b1724d52c9
Manuel Morales <manuelmorales@dhcp-172-24-140-214.eduroam.wireless.private.cam.ac.uk>
2022-02-23 12:29:57 +0000

    Added SIMUnet's extra layer

---
+++ n3fit/src/n3fit/layers/CombineCfac.py
@@ -0,0 +1,32 @@
+import tensorflow as tf 
+from keras.layers import Linear  
+
+class CombineCfacLayer(Linear):
+   """
+   Creates the extra layer to fit SMEFT c-factors
+   on top of NNPDF4.0's architecture.  
+   
+   Parameters
+   ----------
+            ncfacs: int 
+               Defines the number of Wilson 
+               coefficients to be included in a fit. 
+   """
+   
+   def __init__(self, ncfacs):
+      super(Linear, self).__init__()
+
+      init_value = tf.random_normal_initializer()
+      self.w = tf.Variable(
+         initial_value=init_value(shape=(ncfacs,), dtype='float32'),
+         trainable = True
+         )
+   
+   def call(self, inputs, cfactor_values):
+      return (1 + tf.reduce_sum(w[:, tf.newaxis] * cfactor_values, axis=0)) * inputs
+
+                              
+
+
+
+
