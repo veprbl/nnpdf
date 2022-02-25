0746acd4f2d4b346877746df68f132f74147d830
Manuel Morales <manuelmorales@dhcp-172-24-141-45.eduroam.wireless.private.cam.ac.uk>
2022-02-25 15:07:58 +0000

    Added SIMUnets extra layer for post-observable manipulation

--- n3fit/src/n3fit/layers/CombineCfac.py
+++ n3fit/src/n3fit/layers/CombineCfac.py
@@ -3,26 +3,26 @@ from tensorflow.keras.layers import Layer
 
 class CombineCfacLayer(Layer):
    """
-   Creates the extra layer to fit SMEFT c-factors
-   on top of NNPDF4.0's architecture.  
-   
-   Parameters
-   ----------
-            ncfacs: int 
-               Defines the number of Wilson 
-               coefficients to be included in a the fit. 
+   Creates SIMUnet's combination layer.  
    """
    
    def __init__(self, ncfacs):
-      super().__init__()
-
-      init_value = tf.random_normal_initializer()
+      # Initialize a Layer instance
+      super().__init__()    
+      # Create an initializer instance with mean=0. and std=.05 
+      init_value = tf.random_normal_initializer() 
+      # Initialize vector of trainable weights
       self.w = tf.Variable(
          initial_value=init_value(shape=(ncfacs,), dtype='float32'),
          trainable = True
          )
    
    def call(self, inputs, cfactor_values):
+      """
+      The operation that the SIMUnet layer performs on inputs.
+      At this point we only include linear interference with the SMEFT. 
+
+      """
       return (1 + tf.reduce_sum(w[:, tf.newaxis] * cfactor_values, axis=0)) * inputs
 
                               
