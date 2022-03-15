c6a2e80fbdecea3ec7d68559845a1b15e4b8dc1e
andreab1997 <andreab1997@yahoo.it>
2022-03-15 12:21:11 +0100

    First implementation of new make_replica

--- validphys2/src/validphys/pseudodata.py
+++ validphys2/src/validphys/pseudodata.py
@@ -101,7 +101,7 @@ def read_replica_pseudodata(fit, context_index, replica):
     return DataTrValSpec(pseudodata.drop("type", axis=1), tr.index, val.index)
 
 
-def make_replica(groups_dataset_inputs_loaded_cd_with_cuts, replica_mcseed, genrep=True):
+def make_replica(groups_dataset_inputs_loaded_cd_with_cuts, replica_mcseed,  dataset_inputs_t0_covmat_from_systematics, genrep=True):
     """Function that takes in a list of :py:class:`validphys.coredata.CommonData`
     objects and returns a pseudodata replica accounting for
     possible correlations between systematic uncertainties.
@@ -153,72 +153,23 @@ def make_replica(groups_dataset_inputs_loaded_cd_with_cuts, replica_mcseed, genr
     # pseudodata replica
     while True:
         pseudodatas = []
-        special_add = []
-        special_mult = []
-        mult_shifts = []
         check_positive_masks = []
         for cd in groups_dataset_inputs_loaded_cd_with_cuts:
             # copy here to avoid mutating the central values.
             pseudodata = cd.central_values.to_numpy(copy=True)
 
-            # add contribution from statistical uncertainty
-            pseudodata += (cd.stat_errors.to_numpy() * rng.normal(size=cd.ndata))
-
-            # ~~~ ADDITIVE ERRORS  ~~~
-            add_errors = cd.additive_errors
-            add_uncorr_errors = add_errors.loc[:, add_errors.columns=="UNCORR"].to_numpy()
-
-            pseudodata += (add_uncorr_errors * rng.normal(size=add_uncorr_errors.shape)).sum(axis=1)
-
-            # correlated within dataset
-            add_corr_errors = add_errors.loc[:, add_errors.columns == "CORR"].to_numpy()
-            pseudodata += add_corr_errors @ rng.normal(size=add_corr_errors.shape[1])
-
-            # append the partially shifted pseudodata
             pseudodatas.append(pseudodata)
-            # store the additive errors with correlations between datasets for later use
-            special_add.append(
-                add_errors.loc[:, ~add_errors.columns.isin(INTRA_DATASET_SYS_NAME)]
-            )
-            # ~~~ MULTIPLICATIVE ERRORS ~~~
-            mult_errors = cd.multiplicative_errors
-            mult_uncorr_errors = mult_errors.loc[:, mult_errors.columns == "UNCORR"].to_numpy()
-            # convert to from percent to fraction
-            mult_shift = (
-                1 + mult_uncorr_errors * rng.normal(size=mult_uncorr_errors.shape) / 100
-            ).prod(axis=1)
-
-            mult_corr_errors = mult_errors.loc[:, mult_errors.columns == "CORR"].to_numpy()
-            mult_shift *= (
-                1 + mult_corr_errors * rng.normal(size=(1, mult_corr_errors.shape[1])) / 100
-            ).prod(axis=1)
-
-            mult_shifts.append(mult_shift)
-
-            # store the multiplicative errors with correlations between datasets for later use
-            special_mult.append(
-                mult_errors.loc[:, ~mult_errors.columns.isin(INTRA_DATASET_SYS_NAME)]
-            )
-
-            # mask out the data we want to check are all positive
             if "ASY" in cd.commondataproc:
                 check_positive_masks.append(np.zeros_like(pseudodata, dtype=bool))
             else:
                 check_positive_masks.append(np.ones_like(pseudodata, dtype=bool))
 
-        # non-overlapping systematics are set to NaN by concat, fill with 0 instead.
-        special_add_errors = pd.concat(special_add, axis=0, sort=True).fillna(0).to_numpy()
-        special_mult_errors = pd.concat(special_mult, axis=0, sort=True).fillna(0).to_numpy()
-
-
+        covmat = dataset_inputs_t0_covmat_from_systematics
+        shifts = covmat @ rng.normal(size=covmat.shape[1])
         all_pseudodata = (
             np.concatenate(pseudodatas, axis=0)
-            + special_add_errors @ rng.normal(size=special_add_errors.shape[1])
-        ) * (
-            np.concatenate(mult_shifts, axis=0)
-            * (1 + special_mult_errors * rng.normal(size=(1, special_mult_errors.shape[1])) / 100).prod(axis=1)
+            + shifts
         )
-
         if np.all(all_pseudodata[np.concatenate(check_positive_masks, axis=0)] >= 0):
             break
 
